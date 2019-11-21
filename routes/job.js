cca5d9fe858d6ea272e9e5e05c9683933f4840f9
Shitanshu <shitanshu.19p@gmail.com>
2019-11-22 02:37:06 +0530

    Authorization - II

--- routes/job.js
+++ routes/job.js
@@ -4,7 +4,7 @@ const verify = require('../services/verifyToken');
 const Job = require('../model/Job');
 
 router.get('/all', verify, (req, res) => {
-    res.json(Job.find());
+    res.json(req.user);
 });
 
 module.exports = router;
